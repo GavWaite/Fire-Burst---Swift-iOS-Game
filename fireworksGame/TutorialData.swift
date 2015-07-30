23ef2bcdccb94e6fde021d3cb8609443a0afc804
GavWaite <gavwaite@gmail.com>
2015-07-29 21:15:24 -0400

    Just before trying new font

---
+++ fireworksGame/TutorialData.swift
@@ -0,0 +1,14 @@
+//
+//  TutorialData.swift
+//  fireworksGame
+//
+//  Created by Gavin Waite on 29/07/2015.
+//  Copyright (c) 2015 Gavin Waite. All rights reserved.
+//
+
+import Foundation
+
+
+class TutorialData {
+    
+}
