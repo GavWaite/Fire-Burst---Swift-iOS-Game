75b7cc6f3f86550eb2e9d7a980c72cc9bed95245
GavWaite <gavwaite@gmail.com>
2015-08-03 18:22:43 -0400

    Working copy - still pre-howtoplay and before code rewrite

--- fireworksGame/SettingsViewController.swift
+++ fireworksGame/SettingsViewController.swift
@@ -11,18 +11,27 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     var settings: SettingsData?
+    var model: Highscores?
     
     @IBOutlet weak var settingsSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         settings = SettingsData.Static.instance
+        model = Highscores.Static.instance
         settingsSwitch.on = settings!.mutedSound
     }
     
     
     @IBAction func switchToggled(sender: UISwitch) {
         settings!.mutedSound = settingsSwitch.on
+        var saveData = SuperSimpleSave()
+        saveData.savedScores = self.model!.scores
+        saveData.soundMute = self.settings!.mutedSound
+        Persistence.save(saveData)
+        println("Saved settings data")
+
+        
     }
 
 }
